01becfe055e03ade2ad3250eddd0d8e3d7fbd395
zhna123 <cszhna@gmail.com>
2024-09-18 11:20:36 -0400

    added tanstack query

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,15 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, 
+      ...tseslint.configs.recommended,
+      ...pluginQuery.configs['flat/recommended']
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
